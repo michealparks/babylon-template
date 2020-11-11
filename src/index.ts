c34802a77134db81009e011b59c6789ad7ece55d
Micheal Parks <michealparks1989@gmail.com>
2020-11-11 18:26:58 -0500

    add default postprocess pipeline

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import { Engine } from '@babylonjs/core/Engines/engine'
 import { Scene } from '@babylonjs/core/scene'
-import { Vector3 } from '@babylonjs/core/Maths/math'
+import { Vector2, Vector3 } from '@babylonjs/core/Maths/math'
 import { FreeCamera } from '@babylonjs/core/Cameras/freeCamera'
 import { HemisphericLight } from '@babylonjs/core/Lights/hemisphericLight'
+import { SpotLight } from '@babylonjs/core/Lights/spotLight'
 import { Mesh } from '@babylonjs/core/Meshes/mesh'
 import { AmmoJSPlugin } from '@babylonjs/core/Physics/Plugins/ammoJSPlugin'
 import { PhysicsImpostor } from '@babylonjs/core/Physics/physicsImpostor'
 import { SceneLoader } from '@babylonjs/core/Loading'
-import { GLTFLoader } from '@babylonjs/loaders/glTF/2.0/glTFLoader'
+import { DefaultRenderingPipeline } from '@babylonjs/core/PostProcesses/RenderPipeline/Pipelines/defaultRenderingPipeline'
 
 // Required side effects to populate the Create methods on the mesh class.
 // Without this, the bundle would be smaller but the createXXX methods from mesh would not be accessible.
@@ -24,25 +25,29 @@ const canvas = document.getElementById('canvas') as HTMLCanvasElement
 const engine = new Engine(canvas, antialias, engineOptions, adaptToDeviceRatio)
 const scene = new Scene(engine)
 
-// This creates and positions a free camera (non-mesh)
-const camera = new FreeCamera('camera1', new Vector3(-4, 5, 10), scene)
-
-// This targets the camera to scene origin
+const noPreventDefault = true
+const camera = new FreeCamera('camera', new Vector3(-4, 5, 10), scene)
 camera.setTarget(Vector3.Zero())
+camera.attachControl(canvas, noPreventDefault)
 
-// This attaches the camera to the canvas
-camera.attachControl(canvas, true)
+{
+  const name = 'hemisphericLight'
+  const direction = new Vector3(0, 1, 0)
+  const light = new HemisphericLight(name, direction, scene)
+  light.intensity = 0.4
+}
 
-// This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-const light = new HemisphericLight('light1', new Vector3(0, 1, 0), scene)
-
-// Default intensity is 1. Let's dim the light a small amount
-light.intensity = 0.7
+{
+  const name = 'spotLight'
+  const position = new Vector3(0, 3, 0)
+  const direction = new Vector3(0, -1, 0)
+  const angle = Math.PI / 3
+  const exponent = 2
+  const light = new SpotLight(name, position, direction, angle, exponent, scene)
+  light.intensity = 40
+}
 
 // Render every frame
-engine.runRenderLoop(() => {
-  scene.render()
-})
 
 const init = async () => {
   if (import.meta.env.MODE === 'development') {
@@ -60,21 +65,54 @@ const init = async () => {
   const ammoPlugin = new AmmoJSPlugin(useDeltaForWorldStep, Ammo)
   scene.enablePhysics(gravityVector, ammoPlugin)
 
-  const ground = Mesh.CreateGround("ground1", 3.8, 3.8, 2, scene)
-  const sphere = Mesh.CreateSphere("sphere1", 32, 1, scene)
+  {
+    const width = 3.8
+    const height = 3.8
+    const subdivisions = 1
+    const ground = Mesh.CreateGround('ground', width, height, subdivisions, scene)
+    ground.physicsImpostor = new PhysicsImpostor(ground, PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, scene)
+    ground.position.y = -0.01
+  }
 
-  ground.position.y = -0.01
-  
-  sphere.physicsImpostor = new PhysicsImpostor(sphere, PhysicsImpostor.SphereImpostor, { mass: 1, restitution: 0.9 }, scene)
-  ground.physicsImpostor = new PhysicsImpostor(ground, PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, scene)
-  sphere.position.y = 5
+  {
+    const segments = 32
+    const diameter = 1
+    const sphere = Mesh.CreateSphere('sphere', segments, diameter, scene)
+    sphere.physicsImpostor = new PhysicsImpostor(sphere, PhysicsImpostor.SphereImpostor, { mass: 1, restitution: 0.9 }, scene)
+    sphere.position.y = 5
+  }
 
   const gltf = await SceneLoader.AppendAsync('./assets/glb/', 'pixel_room.glb', scene)
 
-  console.log(gltf)
   for (const texture of gltf.textures) {
     texture.updateSamplingMode(8)
   }
+
+  {
+    const hdr = true
+    const pipeline = new DefaultRenderingPipeline('standardPipeline', hdr, scene, [camera])
+    pipeline.samples = 4
+  
+    pipeline.bloomEnabled = true
+    pipeline.bloomThreshold = 0.2
+    pipeline.bloomWeight = 0.3
+    pipeline.bloomKernel = 64
+    pipeline.bloomScale = 0.5
+
+    pipeline.depthOfFieldEnabled = true
+    // distance of the current focus point from the camera in millimeters considering 1 scene unit is 1 meter
+    pipeline.depthOfField.focusDistance = 2000
+    // focal length of the camera in millimeters  
+    pipeline.depthOfField.focalLength = 40
+    // aka F number of the camera defined in stops as it would be on a physical device
+    pipeline.depthOfField.fStop = 1.4
+  }
+
+  // const postProcess = new BABYLON.HighlightsPostProcess("highlights", 1.0, camera)
+
+  engine.runRenderLoop(() => {
+    scene.render()
+  })
 }
 
 init()
